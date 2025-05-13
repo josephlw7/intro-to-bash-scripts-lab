bd810170f36d7817a399075ef84d258e34727595
josephlw7 <vision4thefuture@outlook.com>
2025-05-12 18:05:06 -0700

    add build-project

---
+++ build-project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir build-project
+cd build-project
+touch README.md
+touch build-project.sh
+git init
+git add --chmod=+x ./build-project.sh
+
+
---
+++ first-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is my first Bash script!
+
+name=$1
+
+echo "This is my Bash script!"
+echo "Hello, $name!"
+echo "Today is $(date)."
